3d0a935e702f605b62d8275df4cfa9f484f10b68
Marius Lucacel <marius.lucacel@analog.com>
2026-03-24 12:51:30 +0200

    iio.h: Add flags field to iio_context_params
    Add a flags field to iio_context_params.
    The first flag, IIO_CTX_XML_INCLUDE_VALUES, will allow
    iio_context_get_xml() to include attribute values in the generated XML.
    
    The flags field uses 4 bytes from the reserved space.
    
    Signed-off-by: Marius Lucacel <marius.lucacel@analog.com>

--- include/iio/iio.h
+++ include/iio/iio.h
@@ -118,6 +118,13 @@ enum iio_log_level {
 	LEVEL_DEBUG = 5,
 };
 
+enum iio_context_flags {
+	/** @brief Include values in XML output
+	 * When set, iio_context_get_xml() will read and serialize attribute
+	 * values into the XML output. */
+	IIO_CTX_XML_INCLUDE_VALUES = 1 << 0,
+};
+
 /**
  * @struct iio_context_params
  * @brief IIO context creation information
@@ -155,8 +162,12 @@ struct iio_context_params {
 	 * - Positive values: timeout after specified milliseconds */
 	int timeout_ms;
 
+	/** @brief Flags that control context behavior.
+	 * Bitmask of values from enum iio_context_flags. */
+	unsigned int flags;
+
 	/** @brief Reserved for future fields. */
-	char __rsrv[32];
+	char __rsrv[28];
 };
 
 /*
